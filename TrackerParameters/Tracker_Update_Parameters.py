72e5bc0e5084b79b07c490140f861453efd18cd0
gqxwolf <gongwolf@gmail.com>
2021-06-23 10:29:52 -0600

    update parameters

--- TrackerParameters/Tracker_Update_Parameters.py
+++ TrackerParameters/Tracker_Update_Parameters.py
@@ -47,7 +47,7 @@ def updateParameters(data):
     header.update({'Content-Type': 'application/json'})
     header.update({'accept': 'application/json'})
     header.update({
-        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJTVUJTQ1JJQkVSOjQ1MTk0Il0sImV4cCI6MTYyMjA0NzQxMCwianRpIjoiMTUwNTZkMDEtOWViNS00ODgyLTllYWYtMWIwZGI1YTZlNDM2IiwiY2xpZW50X2lkIjoidHBlLWV1LWFwaS9xaXh1Z29uZ0BubXN1LmVkdSJ9.AnH6uIs-ZC_q3KrwwIBf3EljlWIkvx3XKEcQt_dpHQZ5OHu5XntX92XGMvLulFPSo7QOE5nWfoXRIlWNurt2hA'})
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJTVUJTQ1JJQkVSOjQ1MTk0Il0sImV4cCI6MTYyNDEyNTAwMiwianRpIjoiMzA2OTc3OGItZWJiNC00MGMwLWJjYWQtYWVmMDk5Zjk1MzQyIiwiY2xpZW50X2lkIjoidHBlLWV1LWFwaS9xaXh1Z29uZ0BubXN1LmVkdSJ9.ljwZ7lNmunGmubspNn4DSGnfQSBjZYB23ifR4jU6-8bZqM_hEDfZBNJPWEc7vdsyDK8oG9Xl0dhvNcf2bTnyNA'})
 
     req = requests.post(url, data=data, headers=header)
     # print(req.content)
@@ -85,7 +85,7 @@ if __name__ == "__main__":
             # break
 
             # ## Change the transmission to "Double Random Datarate"
-            data="{\"deviceEUI\": \"" + d + "\",\"trackerCommandType\": \"TrackerCommandToSendRawData\",\"status\": \"QUEUED\",\"rawPayload\": \"0b0a0e00000004\"}"
+            data="{\"deviceEUI\": \"" + d + "\",\"trackerCommandType\": \"TrackerCommandToSendRawData\",\"status\": \"QUEUED\",\"rawPayload\": \"0b0a0e00000003\"}"
             print(data)
             status_code = updateParameters(data)
             print("{}: device: {}   --> status_code:{}".format(index, d, status_code))
--- TrackerParameters/renewToken.py
+++ TrackerParameters/renewToken.py
@@ -8,7 +8,7 @@ def updateParameters(target_url, data):
     header.update({'Content-Type': 'application/x-www-form-urlencoded'})
     header.update({'Accept': 'application/json'})
     header.update({
-        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJTVUJTQ1JJQkVSOjQ1MTk0Il0sImV4cCI6MTYyMzI4MTI5NSwianRpIjoiYWQxMmM2ZjktNGE3Mi00ZTJjLThmYmMtOWYwOWVjY2RmZGFmIiwiY2xpZW50X2lkIjoidHBlLWV1LWFwaS9xaXh1Z29uZ0BubXN1LmVkdSJ9.Rjxzf3UH39oXq-AYBjmU5TMdnAylf4hdN7kceoenEB3KyavYldlQIaRTXoz65sU4W-nk9As6YAbW3Yjc2NgrZg","scope":"[SUBSCRIBER:45194]","jti":"ad12c6f9-4a72-4e2c-8fbc-9f09eccdfdaf'})
+        'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJTVUJTQ1JJQkVSOjQ1MTk0Il0sImV4cCI6MTYyNDEyNTAwMiwianRpIjoiMzA2OTc3OGItZWJiNC00MGMwLWJjYWQtYWVmMDk5Zjk1MzQyIiwiY2xpZW50X2lkIjoidHBlLWV1LWFwaS9xaXh1Z29uZ0BubXN1LmVkdSJ9.ljwZ7lNmunGmubspNn4DSGnfQSBjZYB23ifR4jU6-8bZqM_hEDfZBNJPWEc7vdsyDK8oG9Xl0dhvNcf2bTnyNA'})
 
     req = requests.post(target_url, data=data, headers=header)
     # print(req.content)
